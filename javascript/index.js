3db7168db79e6170fd713289943970f644552a90
LuloDev <ezequielcruz018@gmail.com>
2024-02-01 09:59:43 -0400

    modified:   Python/data.txt 	modified:   javascript/data.txt 	modified:   javascript/index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,4 @@
+//variables
 let firstname = "junior";
 let age = 30;
 let isAproved = true;
@@ -6,3 +7,18 @@ console.log(typeof (firstname))
 console.log(typeof (age))
 console.log(typeof (isAproved))
 
+
+//objeto
+let person = {
+    firstname: "junior",
+    age: 30,
+    isAproved: true
+}
+console.log(person.firstname)
+
+//dot notation
+person.firstname = "Ezequiel"
+console.log(person.firstname)
+// bracket notation
+person["firstname"] = "lulo"
+console.log(person.firstname)
