ad270f15b1d577d2d3418d601c5e4fb944237a85
LuloDev <ezequielcruz018@gmail.com>
2024-03-17 11:59:29 -0400

    new file:   javascript/Switch.js 	new file:   javascript/prorip-ternario.js 	deleted:    javascript/Objectos-literales.js 	deleted:    javascript/README.md 	deleted:    javascript/Switch.js 	deleted:    javascript/Valor-referencia.js 	deleted:    javascript/alerts.js 	deleted:    javascript/app.js 	deleted:    javascript/arreglos.js 	deleted:    javascript/arreglos2.js 	deleted:    javascript/data.txt 	deleted:    javascript/funciones.js 	deleted:    javascript/if-else.js 	deleted:    javascript/index.js 	deleted:    javascript/operador-ternario.js 	deleted:    javascript/palabras-reservadas.js 	deleted:    javascript/primitivos.js 	deleted:    javascript/prorip-ternario.js 	deleted:    javascript/protip-retorno.js

---
+++ javascript/prorip-ternario.js
@@ -0,0 +1,19 @@
+// const elmayor = (a, b) => (a > b) ? a : b;
+// const membresia = (miembro) => (miembro) ? "2 dolares" : "10 dolares";
+
+// console.log(elmayor(10, 15));
+// console.log(membresia(false));
+
+
+const nota = 100;
+const grado = nota >= 95 ? "A+" :
+    nota >= 90 ? "A" :
+        nota >= 95 ? "B+" :
+            nota >= 95 ? "B" :
+                nota >= 95 ? "C+" :
+                    nota >= 95 ? "C" : "F";
+
+console.log({ nota, grado })
+
+
+
