8e923dc19c86a6df281a6b842b895d2e0b0fa30a
LuloDev <ezequielcruz018@gmail.com>
2024-04-30 17:14:12 -0400

    new file:   javascript/leetcode/Longest Common Prefix.js 	javascript/Bases/bom.js 	javascript/Bases/test.html

---
+++ javascript/leetcode/Longest Common Prefix.js
@@ -0,0 +1,21 @@
+/*Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".*/
+
+
+var longestCommonPrefix = function(strs) {
+  
+    if (strs.length === 0) {
+        return ""
+    }
+
+    const prefix = [0];
+
+    for (let i = 0; i < strs.length; i++) {
+        
+    }
+
+
+    return " "
+
+};
