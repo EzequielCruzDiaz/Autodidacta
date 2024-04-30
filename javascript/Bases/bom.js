94da8de63396fcd9b7dda3972c0a3a661126679b
LuloDev <ezequielcruz018@gmail.com>
2024-04-30 17:14:23 -0400

    new file:   javascript/Bases/bom.js 	new file:   javascript/Bases/test.html

---
+++ javascript/Bases/bom.js
@@ -0,0 +1,12 @@
+
+function getbom() {
+    console.log(screen.width)
+    console.log(screen.height)
+    console.log(window.location)
+}
+
+function abrirventana(url){
+    window.open(url)
+}
+
+getbom();
