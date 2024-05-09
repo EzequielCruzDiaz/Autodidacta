cb80f2b756740a5f422f5465f4c6a4c656c9f959
LuloDev <ezequielcruz018@gmail.com>
2024-05-09 11:03:44 -0400

    modified:   Python/Herencia.py new file:   Python/__pycache__/CuentaBancaria.cpython-311.pyc

--- Python/Herencia.py
+++ Python/Herencia.py
@@ -1,14 +1,22 @@
-from CuentaBancaria import Persona
+class persona:
+    def __init__(self,nombre,apellido):
+        self.nombre = nombre
+        self.apellido = apellido
 
-class Empleado(Persona):
+    def mostrar_persona(self):
+        print("Mi nombre es:", self.nombre, "Mi apellido es:", self.apellido)
+
+class Empleado(persona):
     def __init__(self,nombre,edad,salario):
         super().__init__(nombre,edad)
         self.salario = salario
+        
 
 
     def mostrar_salario(self):
         print("El salario de", self.nombre, "es", self.salario)
 
-empleado1 = Empleado("Ezequiel", 25,75000)
+empleado1 = Empleado("Ezequiel", 25, 75000)
 empleado1.saludar()
 empleado1.mostrar_salario()
+empleado1.mostrar_persona()
