ff540c3a949fc1c956a3567dd58911576687a8a9
Ezequiel Jr.Cruz Diaz <83771482+EzequielCruzDiaz@users.noreply.github.com>
2024-06-10 16:58:33 -0400

    Add files via upload

---
+++ Backend/Herencia.cs
@@ -0,0 +1,48 @@
+// using System;
+// namespace Herencia
+// {
+//     class Animal {
+//     public string Comer ( )
+//         {
+//         return "Comiendo";
+//         }
+
+//     public string Atacar () 
+//     {
+//         return "morder";
+//     }
+    
+//     public string Hablar ()
+//     {
+//         return "maullando";
+//     }
+
+// }
+
+
+// class Gato:Animal 
+
+// {
+//     public required string Nombre {get; set;}
+
+//     public string Raza (){
+//         return "American Botail";
+//     }
+// }
+
+// class Program 
+// {
+//     public static void Main (String[]args) 
+//     {
+//          var gato = new Gato() 
+//          {
+//             Nombre = "grey"
+            
+//          };
+
+        
+//         Console.WriteLine($"Ten cuidado, {gato.Nombre} podría {gato.Atacar()}, mientras está {gato.Comer()}.");
+
+//     }
+// }
+// }
---
+++ Backend/Overload.cs
@@ -0,0 +1,43 @@
+// using System;
+// namespace Matematica
+// {
+
+// class Calculadora  ()
+// {
+//     public int suma (int a, int b) 
+//     {
+//         return a + b;
+//     }
+
+//     public double suma (double a, double b ) 
+//     {
+//         return a + b;
+//     }
+
+//     public int suma (int a, int b, int c) {
+//         return a +  b + c;
+//     }
+
+// }
+
+// class Program {
+
+//      static void Main (string[]args) 
+//     {
+//         Calculadora calculadora = new Calculadora();
+
+//         int resultado1 = calculadora.suma(1,2);
+//         double resultado2 = calculadora.suma(3.5,5.9);
+//         int resultado3 = calculadora.suma(4,6);
+
+
+//         Console.WriteLine(resultado1);
+//         Console.WriteLine(resultado2);
+//          Console.WriteLine(resultado3);
+//     }
+
+    
+
+// }
+
+// }
