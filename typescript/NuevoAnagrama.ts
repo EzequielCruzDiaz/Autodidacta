462200cb8ee5ede9a39d9d84cc7cdae9aada497c
ezequiel <ezequielcruz018@gmail.com>
2024-06-28 14:30:31 -0400

    new file:   typescript/NuevoAnagrama.ts

---
+++ typescript/NuevoAnagrama.ts
@@ -0,0 +1,11 @@
+
+
+
+const diccionario = (word1:string, word2:string) => {
+
+    const NuevaPalabra1 = word1.split(" ").join()
+    const NuevaPalabra2 = word2.split("").join()
+
+}
+
+//La logica de un anagrama consiste en obtener informacion, destructurarla, organizarla y luego 
